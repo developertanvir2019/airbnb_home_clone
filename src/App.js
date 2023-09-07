ab1d04ce00892a4006a4d1ae1045ac36e34f78d8
developertanvir2019 <developertanvir2019@gmail.com>
2023-09-07 12:53:02 +0600

    navbar first look done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import "./App.css";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Map from "./components/Map";
 import Navbar from "./components/Navbar";
-import Ratings from "./components/Ratings";
-import RoomDetails from "./components/RoomDetails";
-import ThingsToKnow from "./components/ThingsToKnow";
 
 function App() {
   return (
-    <div className="lg:px-28 xl:px-28 2xl:px-96">
+    <div>
       <Navbar />
-      <Hero />
-      <RoomDetails />
-      <Ratings />
-      <Map />
-      <ThingsToKnow />
+
       <Footer />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 import logo from "../assets/images/logo.png";
-import avatar from "../assets/images/placeholder.jpg";
+import Search from "./navbar/Search";
+import UserMenu from "./navbar/UserMenu";
 
 const Navbar = () => {
   return (
-    <div className="navbar py-4 bg-base-100 border-b-2">
-      <div className="navbar-start">
+    <div className="navbar py-4 bg-base-100 border-b-2 px-10">
+      <div className="navbar-start w-1/3 hidden md:block cursor-pointer">
         <div className="dropdown">
           <img width={102} height={32} src={logo} alt="logo" />
         </div>
       </div>
 
-      <div className="pt-2 relative mx-auto text-gray-600">
-        <input
-          className="p-4 border-2 border-gray-300 h-10 px-5 pr-16 rounded-3xl text-sm focus:outline-none shadow-md"
-          type="search"
-          name="search"
-          placeholder="Start your Search"
-        />
-        <button
-          type="submit"
-          className="absolute -right-2 top-0 mt-3 mr-4 bg-[#ff385c] p-2 rounded-full"
-        >
+      {/* middle part */}
+      {/* <div className="pt-2 mx-auto text-gray-600">
+        <p>Anywhere</p>
+        <p>any week </p>
+        <p>Any guest</p>
+        <button type="submit" className=" bg-[#ff385c] p-2 rounded-full">
           <svg
             className="text-gray-600 h-4 w-4 fill-current text-white"
             xmlns="http://www.w3.org/2000/svg"
@@ -39,8 +35,14 @@ const Navbar = () => {
             <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
           </svg>
         </button>
+      </div> */}
+      <Search />
+      <div className="navbar-end w-1/3">
+        {" "}
+        <UserMenu />
       </div>
-      <div className="navbar-end ali">
+
+      {/* <div className="navbar-end w-1/3">
         <div className="dropdown flex  dropdown-end">
           <div className="flex items-center">
             {" "}
@@ -99,7 +101,7 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
