a0b0793643b07bdd3c959d6442d1e8611dc143e6
developertanvir2019 <developertanvir2019@gmail.com>
2023-09-07 14:46:24 +0600

    category nav design done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import logo from "../../assets/images/logo.png";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Categories from "./Categories";
 
 const Navbar = () => {
   return (
-    <div className="navbar py-4 bg-base-100 border-b-2 px-10">
-      <div className="navbar-start w-1/3 hidden md:block cursor-pointer">
-        <div className="dropdown">
-          <img width={102} height={32} src={logo} alt="logo" />
+    <>
+      <div className="navbar py-4 bg-base-100 border-b-2 px-10">
+        <div className="navbar-start w-1/3 hidden md:block cursor-pointer">
+          <div className="dropdown">
+            <img width={102} height={32} src={logo} alt="logo" />
+          </div>
+        </div>
+
+        <Search />
+        <div className="navbar-end w-1/3">
+          {" "}
+          <UserMenu />
         </div>
       </div>
-
-      <Search />
-      <div className="navbar-end w-1/3">
-        {" "}
-        <UserMenu />
-      </div>
-    </div>
+      <Categories />
+    </>
   );
 };
 
